150b9725afa3279330b99fe08accbac94865fd43
Bohui (Dennis) Moon <bohuim@google.com>
2017-05-31 12:06:29 -0700

    fixed sublime/setup.sh

--- sublime/setup.sh
+++ sublime/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-ln -siF ~/dotfiles/sublime/User ~/"Library/Application Support/Sublime Text 3/Packages"
+SUBL_PACKAGE="$HOME/Library/Application Support/Sublime Text 3/Packages"
+SUBL_USER=$SUBL_PACKAGE/User
+
+rm -r "$SUBL_USER" 2> /dev/null
+ln -s ~/dotfiles/sublime/User "$SUBL_USER"
