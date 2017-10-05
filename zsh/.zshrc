e69c5e0ffae754a06ed7c8f49f08d4e91f22f6ba
Bohui Moon <bohuimoon@gmail.com>
2017-10-04 18:31:32 -0700

    Changes: + .zshrc: ignore EOF to disable accidentally closing session via CTRL-D + path.zsh: added path to Postgres bin + sublime: scroll past the end, and disable One Dark for vue files

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,9 +1,11 @@
 export ZSH=$HOME/.oh-my-zsh
 
 ZSH_CUSTOM=~/dotfiles/zsh/custom
-
 ZSH_THEME="spaceship"
 
 plugins=(git)
+
+# Options
+setopt ignoreeof
 
 source $ZSH/oh-my-zsh.sh
--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -2,5 +2,8 @@
 # NODE
 export PATH=$PATH:$HOME/.n/bin
 
+# Postgres
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+
 # RUST
 export PATH=$PATH:$HOME/.cargo/bin
