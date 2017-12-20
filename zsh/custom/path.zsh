604fc6867c3948be1e2076e3eae066a9265c4ddf
Bohui Moon <bohuimoon@gmail.com>
2017-12-20 15:30:33 +0900

    .

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -4,6 +4,8 @@ export PATH=$PATH:"/Applications/Visual Studio Code.app/Contents/Resources/app/b
 # NODE
 export PATH=$PATH:$HOME/.n/bin
 
+# MySQL
+export PATH=$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS
 # Postgres
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 
