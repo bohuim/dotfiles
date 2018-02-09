e420326ea68dce5b4a49c3186f9406f43ed839ac
Bohui Moon <bohuimoon@gmail.com>
2018-02-09 08:41:23 +0000

    Removed tool specific PATH variables

--- zsh/custom/export.zsh
+++ zsh/custom/export.zsh
@@ -1,5 +1,2 @@
 
 export EDITOR=vim
-
-# n
-export N_PREFIX="$HOME/.n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install
--- zsh/custom/path.zsh
+++
@@ -1,6 +0,0 @@
-
-# NODE
-export PATH=$PATH:$HOME/.n/bin
-
-# RUST
-export PATH=$PATH:$HOME/.cargo/bin
