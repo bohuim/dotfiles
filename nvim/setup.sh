6643f56c61859ece4f0a0b4df52249583d4bc252
Bohui Moon <bohuimoon@gmail.com>
2020-11-01 21:57:06 -0800

    Update nvim setup script

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+mkdir -p ~/.config/nvim
+ls -s $HOME/dotfiles/nvim/init.vim ~/.config/nvim
+
+mkdir -p ~/.local/share
 ln -s $HOME/dotfiles/nvim ~/.local/share/nvim
+
+nvim --headless +PlugInstall +qa
