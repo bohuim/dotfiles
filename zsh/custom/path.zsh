3a827b404f6dab1f39c8aecb8ab328a0d9a335bb
Bohui Moon <bohuimoon@gmail.com>
2020-11-04 20:27:23 -0800

    zsh: Remove macOS configs

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,13 +1,5 @@
-# VSCode
-export PATH=$PATH:"/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-
-# NODE
-# path for n is set in ~/ZSH_CUSTOM/export.sh
-
-# MySQL
-# export PATH=$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS
-# Postgres
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+# bin
+export PATH=$PATH:$HOME/dotfiles/bin
 
 # RUST
-export PATH=$PATH:$HOME/.cargo/bin
+# export PATH=$PATH:$HOME/.cargo/bin
